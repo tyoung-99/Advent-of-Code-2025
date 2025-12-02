774b55a8c2e7c494a03be36b3ce0d80f23b3a27d
Tristan <127442375+tyoung-99@users.noreply.github.com>
2025-12-01 21:34:25 -0500

    Day01

--- src/Day01.java
+++ src/Day01.java
@@ -6,16 +6,38 @@ public class Day01 extends GenericDay {
     }
 
     public Day01() {
-        super(1, true, true);
+        super(1, false, false);
     }
 
     @Override
     public String part1(List<String> input) {
-        return input.toString();
+        int result = 0;
+
+        int rotation = 50;
+        for (String line : input) {
+            rotation += Integer.parseInt(line.replace("L", "-").replace("R", ""));
+            if (rotation % 100 == 0) {
+                result++;
+            }
+        }
+
+        return Integer.toString(result);
     }
 
     @Override
     public String part2(List<String> input) {
-        return input.toString();
+        int result = 0;
+
+        int rotation = 50;
+        for (String line : input) {
+            int newRotation = rotation + Integer.parseInt(line.replace("L", "-").replace("R", ""));
+
+            result += Math.abs((int) (newRotation / 100)) + (newRotation <= 0 && rotation != 0 ? 1 : 0);
+
+            rotation = newRotation % 100;
+            rotation = rotation < 0 ? rotation + 100 : rotation;
+        }
+
+        return Integer.toString(result);
     }
 }
